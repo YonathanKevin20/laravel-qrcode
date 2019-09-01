ee5502adcf5d5ca8e7c25d08b4b957e0bfcba82d
Kevin <yonathan.kevin20@gmail.com>
2019-09-02 00:24:52 +0700

    add plugin

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,14 +3,18 @@ import store from '~/store'
 import router from '~/router'
 import i18n from '~/plugins/i18n'
 import App from '~/components/App'
+import vuetify from '~/plugins/vuetify'
+import swalPlugin from '~/plugins/sweetalert2';
 
 import '~/plugins'
 import '~/components'
 
 Vue.config.productionTip = false
+Vue.use(swalPlugin)
 
 /* eslint-disable no-new */
 new Vue({
+	vuetify,
   i18n,
   store,
   router,
