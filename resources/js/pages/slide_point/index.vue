50db46fe6c62b62ebc5e6844a33d09d47279f0cc
Kevin <yonathan.kevin20@gmail.com>
2019-11-26 10:54:05 +0700

    fix style slide point

--- resources/js/pages/slide_point/index.vue
+++ resources/js/pages/slide_point/index.vue
@@ -1,11 +1,7 @@
 <template>
-  <v-container
-    fluid
-    class="py-0">
-    <transition name="fade" mode="out-in">
-      <router-view />
-    </transition>
-  </v-container>
+  <transition name="fade" mode="out-in">
+    <router-view />
+  </transition>
 </template>
 
 <script>
--- resources/js/pages/slide_point/list.vue
+++ resources/js/pages/slide_point/list.vue
@@ -1,39 +1,44 @@
 <template>
-  <v-row>
-    <v-carousel
-      cycle
-      style="height: 100%">
-      <v-carousel-item v-for="(item, index) in items" :key="index">
-        <v-sheet
-          :color="colors[index]"
-          height="100%">
-          <v-row
-            class="fill-height"
-            align="center"
-            justify="center">
-            <v-card
-              v-for="(child, i) in item" :key="i"
-              color="white"
-              class="pa-md-4 mx-lg-auto"
-              width="250px"
-              outlined>
-              <v-list-item>
-                <v-list-item-content class="black--text text-center title">
-                  <v-list-item-title>{{ child.name }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-              <v-list-item>
-                <v-list-item-content class="black--text text-center title">
-                  <v-list-item-title>{{ child.qty }}</v-list-item-title>
-                </v-list-item-content>
-              </v-list-item>
-            </v-card>
-          </v-row>
-        </v-sheet>
-      </v-carousel-item>
-    </v-carousel>
-  </v-row>
+  <v-carousel
+    style="height: 100%">
+    <v-carousel-item v-for="(item, index) in items" :key="index">
+      <v-sheet
+        :color="colors[index]"
+        height="100%"
+        tile>
+        <v-row
+          style="height: 100vh"
+          class="fill-height"
+          align="center"
+          justify="center">
+          <v-card
+            v-for="(child, i) in item" :key="i"
+            color="white"
+            class="pa-md-4 mx-lg-auto"
+            width="250px"
+            outlined>
+            <v-list-item>
+              <v-list-item-content class="black--text text-center title">
+                <v-list-item-title>{{ child.name }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content class="black--text text-center title">
+                <v-list-item-title>{{ child.qty }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+        </v-row>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
 </template>
+
+<style>
+div.v-responsive.v-image.v-carousel__item {
+  height: 100% !important;
+}
+</style>
 
 <script>
 import axios from 'axios'
